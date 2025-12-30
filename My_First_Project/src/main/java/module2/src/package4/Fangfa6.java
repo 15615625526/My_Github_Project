d7a2b83b2d3aba4658d982ed356831ff0b6bec22
15615625526 <i_want_you520@163.com>
2025-12-30 16:48:37 +0800

    删除所有工程

--- My_First_Project/src/main/java/module2/src/package4/Fangfa6.java
+++
@@ -1,9 +0,0 @@
-package module2.src.package4;
-
-public class Fangfa6 {
-    public static void main(String[] args) {
-//        这是调用不同工程下的sums方法,该方法请见工程autotest中的Fangfa6.
-//        int a = sums(1, 2);
-//        System.out.println(a);
-    }
-}
