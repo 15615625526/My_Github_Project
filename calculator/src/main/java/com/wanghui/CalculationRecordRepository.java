8b306355070f435c135c07498007b88037138a2c
15615625526 <i_want_you520@163.com>
2025-12-31 21:08:06 +0800

    更新

---
+++ calculator/src/main/java/com/wanghui/CalculationRecordRepository.java
@@ -0,0 +1,6 @@
+package com.wanghui;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalculationRecordRepository extends JpaRepository<CalculationRecord, Long> {
+}
