ddcfbe818c3a412061ad4098153fa98d01f45291
15615625526 <i_want_you520@163.com>
2025-12-31 21:09:23 +0800

    删除所有

--- calculator/src/main/java/com/wanghui/CalculationRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wanghui;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalculationRecordRepository extends JpaRepository<CalculationRecord, Long> {
-}
